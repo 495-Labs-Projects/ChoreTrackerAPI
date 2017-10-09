07910b59c521241e67e7c20f63cb29cf8b94818f
Richard Huang <rahuang@andrew.cmu.edu>
2017-10-09 18:51:43 -0400

    Added instructions and code for swagger docs set up and documentation for children controller.

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,4 +1,56 @@
 class ChildrenController < ApplicationController
+    # This is to tell the gem that this controller is an API
+  swagger_controller :children, "Children Management"
+
+  # Each API endpoint index, show, create, etc. has to have one of these descriptions
+
+  # This one is for the index action. The notes param is optional but helps describe what the index endpoint does
+  swagger_api :index do
+    summary "Fetches all Children"
+    notes "This lists all the children"
+  end
+
+  # Show needs a param which is which child id to show.
+  # The param defines that it is in the path, and that it is the Child's ID
+  # The response params here define what type of error responses can be returned back to the user from your API. In this case the error responses are 404 not_found and not_acceptable.
+  swagger_api :show do
+    summary "Shows one Child"
+    param :path, :id, :integer, :required, "Child ID"
+    notes "This lists details of one child"
+    response :not_found
+  end
+
+  # Create doesn't take in the child id, but rather the required fields for a child (namely first_name and last_name)
+  # Instead of a path param, this uses form params and defines them as required
+  swagger_api :create do
+    summary "Creates a new Child"
+    param :form, :first_name, :string, :required, "First name"
+    param :form, :last_name, :string, :required, "Last name"
+    param :form, :active, :boolean, :required, "Active"
+    response :not_acceptable
+  end
+
+  # Update requires the child id but you can also change the first_name and/or last_name of the child.
+  # Again since it takes in an child id, it can be not found.
+  # Also this will have both path and form params
+  swagger_api :update do
+    summary "Updates an existing Child"
+    param :path, :id, :integer, :required, "Child Id"
+    param :form, :first_name, :string, :optional, "First name"
+    param :form, :last_name, :string, :optional, "Last name"
+    param :form, :active, :boolean, :optional, "Active"
+    response :not_found
+    response :not_acceptable
+  end
+
+  # Lastly destroy is just like the rest and just takes in the param path for child id. 
+  swagger_api :destroy do
+    summary "Deletes an existing Child"
+    param :path, :id, :integer, :required, "Child Id"
+    response :not_found
+  end
+
+
   # Controller Code
 
   before_action :set_child, only: [:show, :update, :destroy]
